03f41ba3c3020dfca4f46673b8092d9022de780a
kobra <landex187@gmail.com>
2023-04-08 18:23:43 +0200

    Új 2 paraméteres megoldás- Első -> Kulcsszó Második -> maximum ár csak azt írja ki ami releváns a fájlba

--- jofogas.sh
+++ jofogas.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Check if keyword argument is provided
-if [[ $# -eq 0 ]]; then
-  echo "Usage: $0 keyword"
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <keyword> <max_price>"
   exit 1
 fi
 
 # Encode keyword for URL
 keyword=$(echo "$1" | sed 's/ /+/g')
 url="https://www.jofogas.hu/magyarorszag?q=$keyword"
+max_price=$(($2))
+
 
 # Make HTTP request and save HTML content to file
 function getPage {
@@ -38,36 +40,33 @@ function getWorkingLinks {
   echo "Links saved to workinglinks.csv"
 }
 
-
+# Call loopLinks function with filename argument
 function loopLinks {
   # Check if filename argument is provided
   if [[ $# -eq 0 ]]; then
     echo "Usage: $0 filename"
     exit 1
   fi
-  
   [ -e ads.csv ] && rm ads.csv && echo "Létezett az 'ads.csv' ezért eltávolítottam azt"
   # Read links from file and loop through them
-
 	counter=1
 	while read -r link; do
 	  # Extract price from link using curl and grep
 	  price=$(curl -s "$link" | grep -o '<meta itemprop="price" content="[0-9]*" />' | sed 's/.*content="\([0-9]*\)".*/\1/' | tr -d ' ')
+	  printf "A termék ára: %-15s ------ Az általad megadott ár: %-15s \n" "$price" "$max_price"
 	  date=$(curl -s "$link" | grep -F -w 'class="time"' --text | sed 's/<[^>]*>//g')
-
-	  # Format the link, price, and date with equal columns
-	  printf "%-5s | %-60s | %-10s | %-20s\n" "$counter" "$link" "$price Ft" "$date"
-	  
-	  # Append link, price, and date to output file
-	  printf "%-5s | %-130s | %-10s | %-20s\n" "$counter" "$link" "$price Ft" "$date" >> ads.csv
-	  
-	  ((counter++))
+	   if [ $(($price)) -le $(($max_price)) ]; then
+	    # Format the link, price, and date with equal columns
+	    printf "\n A termék kiiírásra került a listádba \n %-5s | %-60s | %-10s | %-20s\n" "$counter" "$link" "$price Ft" "$date"
+	    
+	    # Append link, price, and date to output file
+	    printf "%-5s | %-130s | %-10s | %-20s\n" "$counter" "$link" "$price Ft" "$date" >> ads.csv
+	    
+	    ((counter++))
+	  fi
 	done < "$1"
 
 }
-
-# Call loopLinks function with filename argument
-
 
 
 # Call the getPage and getPrice functions
@@ -75,4 +74,3 @@ getPage
 getAllLinks
 getWorkingLinks
 loopLinks "workinglinks.csv"
-
