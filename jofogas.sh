2fd86f603684c39615381f4d710c7f551f5b513d
kobra <landex187@gmail.com>
2023-04-08 16:32:33 +0200

    Formázott output

--- jofogas.sh
+++ jofogas.sh
@@ -15,26 +15,11 @@ function getPage {
   curl -s -o jofogas.html "$url"
 }
 
-function getPrice {
-  echo "Price" > prices.csv
-  grep -o '<span data-hj-suppress class="price-value" itemprop="price" content="[0-9]*">[^<]*</span>' jofogas.html | while read -r price_tag; do
-    price=$(echo "$price_tag" | sed 's/.*content="\([0-9]*\)".*/\1/' | tr -d ' ')
-    echo "$price Ft" >> prices.csv
-  done
-  echo "Prices saved to prices.csv"
-}
-
-function getDate {
-  echo "Date" > date.csv
-  grep -o '<div class="time">[^<]*</div>' jofogas.html | while read -r date; do
-    date=$(echo "$date" | sed 's/<[^>]*>//g' | tr -d '[:space:]')
-    echo "$date" >> date.csv
-  done
-  echo "Dates saved to date.csv"
-}
 
 
+#Az összes elsőoldalas link lementése a keresés alapján -> ezek relevánsak nekünk
 function getAllLinks {
+   [ -e alllinks.csv ] && rm alllinks.csv && echo "Létezett az 'alllinks.csv' ezért eltávolítottam azt"
   echo "Link" > links.csv
   grep -o '<a href="[^"]*"' jofogas.html | while read -r link; do
     link=$(echo "$link" | cut -d '"' -f 2)
@@ -44,6 +29,7 @@ function getAllLinks {
 }
 
 function getWorkingLinks {
+  [ -e workinglinks.csv ] && rm workinglinks.csv && echo "Létezett az 'workinglinks.csv' ezért eltávolítottam azt"
   echo "Link" > links.csv
   grep -o '<a href="https://www.jofogas.hu/\(csongrad\|budapest\|gyor_moson_sopron\|baranya\|fejer\|szabolcs_szatmar_bereg\|heves\|pest\|veszprem\)[^"]*"' jofogas.html | while read -r link; do
     link=$(echo "$link" | cut -d '"' -f 2)
@@ -53,13 +39,6 @@ function getWorkingLinks {
 }
 
 
-function getItems {
-    sed -n '/<div class="col-xs-12 box listing list-item gallery-items   has-bottom-line   priorized  reListElement"/,/<\/div>/p' jofogas.html > items.html
-    sed -i 's/^[ \t]*//' items.html
-}
-
-
-
 function loopLinks {
   # Check if filename argument is provided
   if [[ $# -eq 0 ]]; then
@@ -67,21 +46,24 @@ function loopLinks {
     exit 1
   fi
   
+  [ -e ads.csv ] && rm ads.csv && echo "Létezett az 'ads.csv' ezért eltávolítottam azt"
   # Read links from file and loop through them
-  while read -r link; do
-    # Extract price from link using curl and grep
-    price=$(curl -s "$link" | grep -o '<meta itemprop="price" content="[0-9]*" />' | sed 's/.*content="\([0-9]*\)".*/\1/' | tr -d ' ')
-    date=$(curl -s "$link" | grep -F -w 'class="time"' --text | sed 's/.*<span class="time">//g;s/<\/span>.*//g')
 
-    
-    # Print link and price to console
-    echo "$link, $price, $date"
-    
-    # Append link and price to output file
-    echo "$link, $price, $date" >> ads.csv
-    done < "$1"
-  
-    rm "workinglinks.csv"
+	counter=1
+	while read -r link; do
+	  # Extract price from link using curl and grep
+	  price=$(curl -s "$link" | grep -o '<meta itemprop="price" content="[0-9]*" />' | sed 's/.*content="\([0-9]*\)".*/\1/' | tr -d ' ')
+	  date=$(curl -s "$link" | grep -F -w 'class="time"' --text | sed 's/<[^>]*>//g')
+
+	  # Format the link, price, and date with equal columns
+	  printf "%-5s | %-60s | %-10s | %-20s\n" "$counter" "$link" "$price Ft" "$date"
+	  
+	  # Append link, price, and date to output file
+	  printf "%-5s | %-130s | %-10s | %-20s\n" "$counter" "$link" "$price Ft" "$date" >> ads.csv
+	  
+	  ((counter++))
+	done < "$1"
+
 }
 
 # Call loopLinks function with filename argument
@@ -92,6 +74,5 @@ function loopLinks {
 getPage
 getAllLinks
 getWorkingLinks
-getItems
 loopLinks "workinglinks.csv"
 
